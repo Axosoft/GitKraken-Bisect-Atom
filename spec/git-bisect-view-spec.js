e672e21e8805cb8d3a64686b022f5522a23f9bd5
Tyler Wanek <tylerw@axosoft.com>
2018-05-21 09:57:10 -0700

    Initial Commit

---
+++ spec/git-bisect-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GitBisectView from '../lib/git-bisect-view';
+
+describe('GitBisectView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
