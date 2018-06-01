1b0591501129244bbf57bac18880315cb770e447
Brandon Quan <brandonq@axosoft.com>
2018-06-01 09:42:08 -0700

    Add post-install script

---
+++ postInstall.js
@@ -0,0 +1,100 @@
+const checksum = require('checksum');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const path = require('path');
+const request = require('request');
+const tar = require('tar');
+const url = require('url');
+
+const config = {
+    expectedChecksum: '',
+    source: '',
+    gitRsFile: '',
+    vendorDirectory: path.join(process.cwd(), 'vendor')
+};
+
+//Need to change config.source from Steve's fork to main fork
+switch(process.platform){
+    case 'win32':
+        config.expectedChecksum = 'dfa06f31de69963c7d0a8c5130cc835631294f72f31a99bc7c13eb43db655883'
+        config.source = url.parse(
+            'https://github.com/stevek-axo/git-rs/releases/download/0.1.0/x86_64-pc-windows-msvc.tar.gz'
+        );
+        config.gitRsFile = 'x86_64-pc-windows-msvc.tar.g';
+        break;
+    case 'linux':
+        config.expectedChecksum = '21e3ca9ff5bd10fb91c6e2c59df3657921afbc5095b350161b015169e08d731b'
+        config.source = url.parse(
+            'https://github.com/stevek-axo/git-rs/releases/download/0.1.0/x86_64-unknown-linux-gnu.tar.gz'
+        )
+        config.gitRsFile = 'x86_64-unknown-linux-gnu.tar.gz';
+        break;
+    case 'darwin':
+        config.expectedChecksum = 'c9036e36e23df3065b6842f17419d9417454f33fe6c87871141746284b1a7a85'
+        config.source = url.parse(
+            'https://github.com/stevek-axo/git-rs/releases/download/0.1.0/x86_64-apple-darwin.tar.gz'
+        )
+        config.gitRsFile = 'x86_64-apple-darwin.tar.gz';
+        break;
+}
+
+const getFileChecksum = async (filePath) => new Promise((resolve) => {
+    checksum.file(filePath, { algorithm: 'sha256' } , (_, hash) => resolve(hash));
+  });
+
+const unpackFile = async (filePath, destinationPath) => tar.extract({
+    cwd: destinationPath,
+    file: filePath
+  });
+
+const setupGitRs = (config) => {
+    const options = {
+        url: config.source
+    };
+    const req = request.get(options);
+    req.pipe(fs.createWriteStream(config.gitRsFile));
+
+    req.on('error', (error) => {
+        console.log('Failed to fetch gitrs');
+        process.exit(1);
+    });
+
+    req.on('response', (res) => {
+        if (res.statusCode !== 200) {
+            console.log(`Non-200 response returned from ${config.source.toString()} - (${res.statusCode})`);
+            process.exit(1);
+        }
+    })
+
+    req.on('end', async () => {
+        const checksum = await getFileChecksum(config.gitRsFile, config);
+        if (checksum != config.expectedChecksum) {
+            console.log('Checksum validation failed');
+            process.exit(1);
+        }
+
+        mkdirp(config.vendorDirectory, (error) => {
+            if (error) {
+              console.log('Could not create vendor directory');
+              process.exit(1);
+            }
+        });
+
+        try {
+            await unpackFile(path.join(process.cwd(), config.gitRsFile), config.vendorDirectory);
+        } catch (error) {
+            console.log('Could not extract gitrs archive');
+            process.exit(1);
+        }
+
+        fs.unlink(config.gitRsFile, (error) => {
+            if (error) {
+                console.log('Could not delete gitrs tar file');
+                process.exit(1);
+            }
+        });
+    });
+}
+
+setupGitRs(config);
+
