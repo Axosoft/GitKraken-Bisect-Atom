84171972f43ce45f8e93d7ded82faf8c84b741ab
Steven King Jr <stevek@axosoft.com>
2018-06-01 10:05:28 -0700

    Update release checksums for Windows & Mac

--- postInstall.js
+++ postInstall.js
@@ -16,24 +16,24 @@ const config = {
 //Need to change config.source from Steve's fork to main fork
 switch(process.platform){
     case 'win32':
-        config.expectedChecksum = 'dfa06f31de69963c7d0a8c5130cc835631294f72f31a99bc7c13eb43db655883'
+        config.expectedChecksum = '77507cfc97cf52437422d061097aec9ec33a06c26431df4f4b2286cf413da9f4';
         config.source = url.parse(
             'https://github.com/stevek-axo/git-rs/releases/download/0.1.0/x86_64-pc-windows-msvc.tar.gz'
         );
         config.gitRsFile = 'x86_64-pc-windows-msvc.tar.g';
         break;
     case 'linux':
-        config.expectedChecksum = '21e3ca9ff5bd10fb91c6e2c59df3657921afbc5095b350161b015169e08d731b'
+        config.expectedChecksum = '21e3ca9ff5bd10fb91c6e2c59df3657921afbc5095b350161b015169e08d731b';
         config.source = url.parse(
             'https://github.com/stevek-axo/git-rs/releases/download/0.1.0/x86_64-unknown-linux-gnu.tar.gz'
-        )
+        );
         config.gitRsFile = 'x86_64-unknown-linux-gnu.tar.gz';
         break;
     case 'darwin':
-        config.expectedChecksum = 'c9036e36e23df3065b6842f17419d9417454f33fe6c87871141746284b1a7a85'
+        config.expectedChecksum = '9c5e372783d404ef49469370150c967efc3d31527f16ea08b5911f913c11b531';
         config.source = url.parse(
             'https://github.com/stevek-axo/git-rs/releases/download/0.1.0/x86_64-apple-darwin.tar.gz'
-        )
+        );
         config.gitRsFile = 'x86_64-apple-darwin.tar.gz';
         break;
 }
